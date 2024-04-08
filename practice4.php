b515edfde91c0ef629fdcc9a7994bcd050e93867
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-04-07 21:44:20 -0500

    Update practice4.php

--- practice4.php
+++ practice4.php
@@ -1,23 +1,34 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Grid</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="">
+<style>
+</style>
 <body>
 
+<div class="">
 <?php
-$sz = $_POST["size"]; 
-echo "<table>";
-for ($i=1; $i <= $sz; $i++) { 
-    echo "<tr> \n";
-    
-    for ($j=1; $j <= $sz; $j++) { 
-        $n = $i * $j;
-        
-        echo "<td>$n</td> \n";
-    }
-    
-    echo "</tr>";
+echo "<table border=\"i\"><tbody>";
+echo "<tr><td></td>";
+
+for ($y = 1; $y < $_POST["num"]; $y++) {
+	echo "<td>" . $y . "</td>";
 }
+echo "</tr>";
 
-echo "</table>";
+  for ($y = 1; $y < $_POST["num"]; $y++) {
+	  echo "<tr>";
+	  echo "<td>" . $y . "</td>";
+	  for ($x = 1; $x < $_POST["num"]; $x++) {
+		echo "<td>" . $x * $y . "</td>";
+	  }
+	  echo "</tr>";
+  }
+  echo "</table></tbody>";
+ ?>
+</div>
 
-?>
 </body>
 </html>
