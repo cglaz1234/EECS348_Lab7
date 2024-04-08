8edc7dbce7846c9ccfa1b47c97fe5be68c978365
cglaz1234 <157721300+cglaz1234@users.noreply.github.com>
2024-04-07 21:49:24 -0500

    Update practice4.php

--- practice4.php
+++ practice4.php
@@ -10,24 +10,29 @@
 
 <div class="">
 <?php
-echo "<table border=\"i\"><tbody>";
-echo "<tr><td></td>";
-
-for ($y = 1; $y <= $_POST["num"]; $y++) {
-	echo "<td>" . $y . "</td>";
-}
-echo "</tr>";
-
-  for ($y = 1; $y <= $_POST["num"]; $y++) {
-	  echo "<tr>";
-	  echo "<td>" . $y . "</td>";
-	  for ($x = 1; $x < $_POST["num"]; $x++) {
-		echo "<td>" . $x * $y . "</td>";
-	  }
-	  echo "</tr>";
-  }
-  echo "</table></tbody>";
- ?>
+    // Check if $_POST["num"] is set and not empty
+    if(isset($_POST["num"]) && !empty($_POST["num"])) {
+        // Echo the table structure
+        echo "<table border=\"1\"><tbody>";
+        echo "<tr><td></td>";
+        for ($y = 1; $y <= $_POST["num"]; $y++) {
+            echo "<td>" . $y . "</td>";
+        }
+        echo "</tr>";
+        for ($y = 1; $y <= $_POST["num"]; $y++) {
+            echo "<tr>";
+            echo "<td>" . $y . "</td>";
+            for ($x = 1; $x <= $_POST["num"]; $x++) {
+                echo "<td>" . $x * $y . "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</tbody></table>";
+    } else {
+        // If $_POST["num"] is not set or empty, display an error message
+        echo "Please provide a valid number.";
+    }
+    ?>
 </div>
 
 </body>
